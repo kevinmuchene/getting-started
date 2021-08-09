b591587b088c84b168dbd21c14e872f6f34b481d
Kevin Muchene <muchenekevin@gmail.com>
2021-08-09 19:32:32 +0300

    This is my comments

---
+++ Git.js
@@ -0,0 +1,32 @@
+/**
+ *
+ * Learn Git today
+ *
+ *
+ * Git is the most popular
+ *
+ *
+ * VCS --> records our changes made over time and stores in a special database repository
+ *
+ * local repository -- machine
+ *
+ * remote --- github, bitbucket etc
+ *
+ * Configure out git
+ *
+ * kevin, sunny
+ *
+ * system -- all users
+ * global -- all repository of the current of the user -- kevin
+ * local -- the current repository --
+ *
+ * track
+ * anything
+ *
+ * lets add something
+ *
+ * const add = () => {}
+ *
+ *
+ *
+ */
